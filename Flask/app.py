ec7fb11ddcd0c1743ab08c042980aeb80be096c7
sunshine <solitudelabhacking@gmail.com>
2018-10-23 16:50:43 -0300

    v

--- Flask/app.py
+++ Flask/app.py
@@ -14,14 +14,16 @@ def var(): # essa função poderia ter qualquer nome
 	v = ['1', '2' ,'3']
 	return render_template("index.html", names=names) 
 
-@app.route("/vrau", methods=["POST"]) # criando uma rota que aceita apenas o método post
-def vrau():
-	num1 = int(request.form.get("num1")) # pegando o valor do form pelo name
-	num2 = int(request.form.get("num2")) # utilizando a função int para converter para inteiro
-	result = num1 + num2
-	return render_template("hello.html", result=result) # render template recebe como argumento o arquivo html e as variáveis que serão impressas
+@app.route("/vrau", methods=["GET", "POST"]) # criando uma rota e definindo os métodos que serão aceitos
+	if request.method == "GET": # request.method para receber o tipo de requisição http que esta sendo feita
+		return "Preencha os valores no formulário"
+	else:
+		num1 = int(request.form.get("num1")) # pegando o valor do form pelo name
+		num2 = int(request.form.get("num2")) # utilizando a função int para converter para inteiro
+		result = num1 + num2
+		return render_template("hello.html", result=result) # render template recebe como argumento o arquivo html e as variáveis que serão impressas
 
 @app.route("/<int:num>") #  recebendo um valor por get
 def numero(num):
-	r = num * num
+	r = num * num2
 	return f"<h1>A raiz quadrada de { num } é { r }</h1>"
