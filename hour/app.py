318d0c17c114b6bea78a0938569dc906cfcf8a0b
sunshine <solitudelabhacking@gmail.com>
2018-10-23 17:30:07 -0300

    v

--- hour/app.py
+++ hour/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request #render para renderizar o html, request para receber a requisição http
 
 app = Flask(__name__)
 
@@ -6,29 +6,33 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html") 
 
-@app.route("/result", methods=["GET", "POST"])
+@app.route("/result", methods=["GET", "POST"]) # definindo os métodos que são aceitos
 def result():
-	h1 = int(request.form.get("h1"))
-	m1 = int(request.form.get("m1"))
-	s1 = int(request.form.get("s1"))
-	h2 = int(request.form.get("h2"))
-	m2 = int(request.form.get("m2"))
-	s2 = int(request.form.get("s2"))
+	if request.method == "GET":
+		return "Preencha os dados do formulário como uma pessoa civilizada em <a href='/'>aqui</a>" # se o metodo for get, mostra essa msg
 
-	h_result = h1 + h2
-	m_result = m1 + m2
-	s_result = s1 + s2
-	d_result = 0
+	else:
+		h1 = int(request.form.get("h1"))
+		m1 = int(request.form.get("m1"))
+		s1 = int(request.form.get("s1"))
+		h2 = int(request.form.get("h2"))
+		m2 = int(request.form.get("m2"))
+		s2 = int(request.form.get("s2"))
 
-	if s_result >= 60:
-		s_result-= 60
-		m_result+= 1
+		h_result = h1 + h2
+		m_result = m1 + m2
+		s_result = s1 + s2
+		d_result = 0
 
-	if m_result >= 60:
-		m_result-= 60
-		h_result+= 1
+		if s_result >= 60:
+			s_result-= 60
+			m_result+= 1
 
-	if h_result > 24:
-		h_result-= 24
-		d_result+= 1
-	return render_template("result.html", h1=h1, m1=m1, s1=s1, h2=h2, m2=m2, s2=s2, rh=h_result, rm=m_result, rs=s_result, d=d_result )
+		if m_result >= 60:
+			m_result-= 60
+			h_result+= 1
+
+		if h_result > 24:
+			h_result-= 24
+			d_result+= 1
+		return render_template("result.html", h1=h1, m1=m1, s1=s1, h2=h2, m2=m2, s2=s2, rh=h_result, rm=m_result, rs=s_result, d=d_result )
