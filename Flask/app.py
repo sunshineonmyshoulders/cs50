fbd98e487593a6f86e7a4b08b41fe1858eec66f1
sunshine <solitudelabhacking@gmail.com>
2018-10-23 00:45:37 -0300

    v

--- Flask/app.py
+++ Flask/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,13 @@ def index(): # essa função poderia ter qualquer nome
 
 @app.route("/var")
 def var(): # essa função poderia ter qualquer nome
-	vrau = "KKKKKKJ"
-	return render_template("index.html", headline=vrau) 
+	names = ["alex", "pamela", "mayco"]
+	v = ['1', '2' ,'3']
+	return render_template("index.html", names=names) 
 
+@app.route("/vrau", methods=["POST"]) # criando uma rota que aceita apenas o método post
+def vrau():
+	num1 = int(request.form.get("num1")) # pegando o valor do form pelo name
+	num2 = int(request.form.get("num2")) # utilizando a função int para converter para inteiro
+	result = num1 + num2
+	return render_template("hello.html", result=result) # render template recebe como argumento o arquivo html e as variáveis que serão impressas
